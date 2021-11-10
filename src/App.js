7c1cf082b3786c4d258979ea5ce83451dec54114
Jacob <white.jacob.dev@gmail.com>
2021-11-09 19:51:20 -0500

    Changed API call to be based on user location

--- src/App.js
+++ src/App.js
@@ -2,28 +2,39 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+  var latitude;
+  var longitude;
   const [restaurants, setRestaurants] = useState([]);
+  
+  function success(position){
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    console.log("("+latitude+", "+longitude+")");
+  }
+
+  navigator.geolocation.getCurrentPosition(success);
+
 
   useEffect(() => {
     axios
       .get(
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&key=AIzaSyDoY5Ziii3WZJ6oNcw2loYOzeZSB09cmNU"
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+latitude+"%2C"+longitude+"&radius=1500&type=restaurant&key=AIzaSyDoY5Ziii3WZJ6oNcw2loYOzeZSB09cmNU"
       )
       .then((result) => {
         for (let i=0; i<result.data.results.length; i++){
           setRestaurants(oldArray => [...oldArray, result.data.results[i].name])
         }
-        console.log(restaurants);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  });
 
   return (
     <div>
-      {restaurants.map(restaurantName => 
-        <div>{ restaurantName }</div>  
+      {restaurants.map((restaurantName, index) => 
+        <div key={index}>{ restaurantName }</div>  
       )}
     </div>
   );
